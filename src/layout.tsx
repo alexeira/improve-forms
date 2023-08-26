07c2e5494d9d1eedf1ead9fe84976c41e6355073
alexeira <alex.pincheira06@gmail.com>
2023-08-25 23:43:02 -0400

    style(format): using prettier-tailwind

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,6 +1,6 @@
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="max-w-xl mx-auto min-h-screen text-gray-50 grid grid-rows-[60px,1fr,60px] gap-2 p-4">
+    <div className="mx-auto grid min-h-screen max-w-xl grid-rows-[60px,1fr,60px] gap-2 p-4 text-gray-50">
       <header>
         <h1 className="text-2xl font-bold">Improve forms 📜</h1>
       </header>
